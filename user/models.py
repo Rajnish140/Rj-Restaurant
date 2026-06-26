beb3f0a81077772ebc9a2d25f9016048b19203f1
Rajnish <rajvanshirjrajbhar@gmail.com>
2026-06-26 07:39:13 +0530

    update views

--- user/models.py
+++ user/models.py
@@ -8,22 +8,24 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.dish_name
-class Customer(models.Model):
-    name=models.CharField(max_length=255)
-    phone=models.CharField(max_length=10)
-    table_no=models.IntegerField()
 
-    def __str__(self):
-        return self.name
     
 class Order(models.Model):
-    customer=models.ForeignKey(Customer,on_delete=models.CASCADE, null=True)
     dish_name = models.CharField(max_length=500)
     price = models.IntegerField()
     Number_of_plate = models.IntegerField(null=True,blank=True)
     order_time = models.DateTimeField(auto_now_add=True, null=True)
     def __str__(self):
         return self.dish_name
+    
+class Customer(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,null=True)
+    name=models.CharField(max_length=255)
+    phone=models.CharField(max_length=10)
+    table_no=models.IntegerField()
+
+    def __str__(self):
+        return self.name
 # Create your models here.
 #class Manager(models.Model):
 #    name=models.CharField(max_length=255,default="")
